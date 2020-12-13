d7fff8f530ed3b49d9d4d5920292a6bd5394df33
Nelly Ramirez <nella.rg25@gmail.com>
2020-12-13 15:18:01 -0600

    production

--- api/config/env/production.js
+++ api/config/env/production.js
@@ -336,7 +336,7 @@ module.exports = {
   * this, just try deploying without setting it and see if it works.)       *
   *                                                                         *
   ***************************************************************************/
-  port: process.env.PORT,
+  // port: 80,
 
 
 
